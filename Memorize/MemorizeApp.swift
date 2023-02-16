5c00ac2c5240e80a6b77ee7efe1ee21de01aa1e7
Abdurakhmon <abdur@basemap.com>
2023-02-16 21:30:19 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Workspace on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

213f098545c4bb5e3bae9da36789ccbbc1315f8e
Abdurakhmon Jamoliddinov <jamoliddinovabdu@gmail.com>
2023-03-07 16:47:29 +0500

    end of lecture 6

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -6,24 +6,26 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: widthThatBestFits()))], content: {
-                    ForEach(game.cards) { card in
-                        CardView(card: card)
-                            .aspectRatio(2/3, contentMode: .fit)
-                            .onTapGesture {
-                                game.choose(card)
-                            }
-                    }.foregroundColor(.red)                
-                })
-            }            
+            AspectVGrid(items: game.cards, aspectRatio: 2/3, content: { card in
+                cardView(for: card)
+            })
+            .foregroundColor(.red)
             .padding()
             .navigationTitle("Memorize")
         }
     }
     
-    func widthThatBestFits() -> CGFloat {
-        return 500/CGFloat(game.cards.count)
+    @ViewBuilder
+    private func cardView(for card: EmojiMemoryGame.Card) -> some View {
+        if card.isMatch && !card.isFaceUp {
+            Rectangle().opacity(0)
+        } else {
+            CardView(card: card)
+                .padding(4)
+                .onTapGesture {
+                    game.choose(card)
+                }
+        }
     }
 }
 
@@ -38,6 +40,9 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+//                    Circle().padding(4).opacity(0.5)
+                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 190-90))
+                        .padding(4).opacity(0.5)
                     Text("\(card.content)").font(font(geometry.size))
                 } else if card.isMatch {
                     shape.opacity(0)
@@ -53,9 +58,9 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 20
+        static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+        static let fontScale: CGFloat = 0.7
     }
 }
 
@@ -104,6 +109,7 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        EmojiMemoryGameView(game: game)
+        game.choose(game.cards[0])
+        return EmojiMemoryGameView(game: game)
     }
 }
